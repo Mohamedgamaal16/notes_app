c56688ef4af73ae1cc9bced5d3d5c186cc44a794
mohamedgamaal16 <Mahooomar@yahoo.com>
2024-01-03 01:30:38 +0200

    change theme and creates notes view

---
+++ lib/views/notes_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
+static String id = 'NotesView' ;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
